b1edf99e439a3b5c208803f1452d3b70ed913e88
Amin Mousavi <mousavi.amin@proton.me>
2026-03-22 17:48:04 +0100

    Use a path parameter to fetch a post

--- src/routes/portal/posts_.$postId.tsx
+++ src/routes/portal/posts_.$postId.tsx
@@ -1,11 +1,37 @@
 import { createFileRoute } from "@tanstack/react-router";
+import axios from "axios";
+import ErrorFound from "../../components/ErrorFound";
+import Pending from "../../components/Pending";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
 
 export const Route = createFileRoute("/portal/posts_/$postId")({
   component: RouteComponent,
+  loader: async ({ params, abortController }) => {
+    const { data } = await axios.get<Post>(
+      "https://jsonplaceholder.typicode.com/posts/" + params.postId,
+      { signal: abortController.signal },
+    );
+
+    return data;
+  },
+  errorComponent: ErrorFound,
+  pendingComponent: Pending,
+  pendingMs: 250,
+  pendingMinMs: 100,
 });
 
 function RouteComponent() {
-  const { postId } = Route.useParams();
+  const post = Route.useLoaderData();
 
-  return <div>Hello "/portal/posts/{postId}"!</div>;
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
 }
