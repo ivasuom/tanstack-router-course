763f792d49b19c725072e8e670999f41d97af6a2
Amin Mousavi <mousavi.amin@proton.me>
2026-03-22 18:22:25 +0100

    Exercise - Fetch & show todos

--- src/routes/portal/todos.tsx
+++ src/routes/portal/todos.tsx
@@ -1,9 +1,38 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import axios from "axios";
 
-export const Route = createFileRoute('/portal/todos')({
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+export const Route = createFileRoute("/portal/todos")({
   component: RouteComponent,
-})
+  loader: async ({ abortController }) => {
+    const { data } = await axios.get<Todo[]>(
+      "https://jsonplaceholder.typicode.com/todos",
+      { signal: abortController.signal },
+    );
+
+    return data;
+  },
+});
 
 function RouteComponent() {
-  return <div>Hello "/portal/todos"!</div>
+  const todos = Route.useLoaderData();
+
+  return (
+    <ul className="list-group">
+      {todos.map((todo) => (
+        <li
+          className="list-group-item d-flex justify-content-between"
+          key={todo.id}
+        >
+          <span>{todo.title}</span>
+          <span>{todo.completed ? "Completed" : "Todo"}</span>
+        </li>
+      ))}
+    </ul>
+  );
 }
