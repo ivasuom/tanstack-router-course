35865afcc31e12306b427557371b17251526d319
Amin Mousavi <mousavi.amin@proton.me>
2026-03-24 18:37:25 +0100

    Validate search params

--- src/routes/portal/products.tsx
+++ src/routes/portal/products.tsx
@@ -1,7 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
+import z from "zod";
+
+const schema = z.object({
+  query: z.string().trim(),
+  color: z.array(z.enum(["black", "gray", "white"])),
+});
 
 export const Route = createFileRoute("/portal/products")({
   component: RouteComponent,
+  validateSearch: schema,
 });
 
 function RouteComponent() {
