63a594d22f709082a7e5fca2ebf9f2aa4f0ff7eb
Amin Mousavi <mousavi.amin@proton.me>
2026-03-29 16:18:14 +0200

    Stale-while-revalidate

--- src/router.tsx
+++ src/router.tsx
@@ -21,6 +21,7 @@ export const router = createRouter({
   defaultPendingComponent: Pending,
   defaultPendingMs: 250,
   defaultPendingMinMs: 100,
+  defaultStaleTime: 1000 * 60 * 5,
 });
 
 type RouterType = typeof router;
