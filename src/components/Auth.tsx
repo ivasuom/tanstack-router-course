936e82de2d5ee62d0eb07c2a0238b1833a07968b
Amin Mousavi <mousavi.amin@proton.me>
2026-03-29 21:52:19 +0200

    Build a simple login form

---
+++ src/components/Auth.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const Auth = () => {
+  const [username, setUsername] = useState("");
+
+  return (
+    <>
+      <h1 className="mb-4">Log in</h1>
+      <div>
+        <input
+          type="text"
+          className="form-control mb-2"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            console.log("Log in " + username);
+            setUsername("");
+          }}
+        >
+          Log in
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Auth;
--- src/routes/profile/index.tsx
+++ src/routes/profile/index.tsx
@@ -1,9 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import Auth from "../../components/Auth";
 
-export const Route = createFileRoute('/profile/')({
+export const Route = createFileRoute("/profile/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/profile/"!</div>
+  return <Auth />;
 }
